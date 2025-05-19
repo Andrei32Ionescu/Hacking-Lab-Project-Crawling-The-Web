8c9f94a1abe542a9d4f12b6c8715ca1289241d3c
Stefan Stoicescu <Stoicescu@student.tudelft.nl>
2025-05-19 19:52:36 +0200

    changed crawler, added function to (sorta) complete checkboxes

--- crawler/stealth_crawler.py
+++ crawler/stealth_crawler.py
@@ -1,37 +1,21 @@
 import asyncio, random, os
-from playwright.async_api import async_playwright, Browser, BrowserContext, Page
-from playwright_stealth import stealth_async
-from fake_useragent import UserAgent
+from utils import handle_cloudflare_challenge
+from camoufox.async_api import AsyncCamoufox
+from browserforge.injectors.playwright import AsyncNewContext
+from playwright.async_api import async_playwright
 
 SCREENSHOT_DIR = "screenshots"
 
 async def grab(url: str, outfile: str) -> None:
-    ua = UserAgent().random
-    async with async_playwright() as p:
-        browser: Browser = await p.firefox.launch(
-            headless=True,
-            args=[
-                "--disable-blink-features=AutomationControlled",
-                "--disable-infobars"
-            ],
-        )
-        context: BrowserContext = await browser.new_context(
-            user_agent=ua,
-            locale="en-US",
-            timezone_id="Europe/Amsterdam",
-            viewport={"width": random.randint(1200, 1920),
-                      "height": random.randint(700, 1080)},
-            device_scale_factor=1.0,
-        )
-        page: Page = await context.new_page()
-        await stealth_async(page)
-        try:
+        async with AsyncCamoufox(
+        headless=False,
+        os=["windows","macos","linux"],
+        ) as browser:
+            page = await browser.new_page()
             await page.goto(url, wait_until="networkidle")
+            print("title:", await page.title())
+            await handle_cloudflare_challenge(page, url, options={"verbose": True})
             await page.screenshot(path=outfile, full_page=True)
-            print(f"Captured {url} in {outfile}")
-        except Exception as e:
-            print(f"Failed to grab {url}: {e}")
-        await browser.close()
 
 async def main():
     os.makedirs(SCREENSHOT_DIR, exist_ok=True)
