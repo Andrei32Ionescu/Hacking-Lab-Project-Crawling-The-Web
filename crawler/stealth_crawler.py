c32b9a86bd4cf54fae92fa919c7654c04ca46556
mateimone <m.t.ivan-1@student.tudelft.nl>
2025-05-19 20:39:09 +0200

    multithreading

--- crawler/stealth_crawler.py
+++ crawler/stealth_crawler.py
@@ -3,36 +3,48 @@ from utils import handle_cloudflare_challenge
 from camoufox.async_api import AsyncCamoufox
 from browserforge.injectors.playwright import AsyncNewContext
 from playwright.async_api import async_playwright
+import multiprocessing, time
 
 SCREENSHOT_DIR = "screenshots"
 
 async def grab(url: str, outfile: str) -> None:
         async with AsyncCamoufox(
-        headless=False,
+        headless=True,
         os=["windows","macos","linux"],
         ) as browser:
             page = await browser.new_page()
-            await page.goto(url, wait_until="networkidle")
-            print("title:", await page.title())
+            try:
+                await page.goto(url, wait_until="domcontentloaded", timeout=10000)
+                await asyncio.sleep(1)
+            except Exception as e:
+                print(f"Timeout error for site: {url}")
+                return
+            # print("title:", await page.title())
             await handle_cloudflare_challenge(page, url, options={"verbose": True})
             await page.screenshot(path=outfile, full_page=True)
+            # k = 1|000|000|000
 
-async def main():
+def main():
     os.makedirs(SCREENSHOT_DIR, exist_ok=True)
 
+    num_cores = multiprocessing.cpu_count()
+    print(f"Running on {num_cores} cores")
     tasks = []
     with open("urls.txt", "r") as f:
         urls = f.readlines()
         for raw_url in urls:
             raw_url = raw_url.strip()
-            if raw_url:
-                full_url = "https://www." + raw_url
-                outfile = os.path.join(SCREENSHOT_DIR, f"{raw_url}.png")
-                tasks.append(grab(full_url, outfile))
-            else:
-                print("Empty URL found, skipping...")
+            full_url = "https://www." + raw_url
+            tasks.append(full_url)
+    with multiprocessing.Pool(num_cores-1) as pool:
+        pool.map(sync_grab, tasks)
 
-    await asyncio.gather(*tasks)
+def sync_grab(full_url: str):
+    url = full_url.split("https://www.")[-1]
+    print(full_url)
+    asyncio.run(grab(full_url, f"screenshots/{url}.png"))
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    timer = time.time()
+    main()
+    print(f"Total time: {time.time() - timer}")
